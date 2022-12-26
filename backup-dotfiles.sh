bfb5aa5aba7f0b9c2e13f66e0e45e4af6921cffd
Mongark <mongarkprogrammer@gmail.com>
2022-12-26 10:59:41 -0400

    Updated dotfiles

--- backup-dotfiles.sh
+++ backup-dotfiles.sh
@@ -3,12 +3,16 @@ rm .vimrc.plug
 rm .bashrc
 rm .xmobarrc
 rm xmonad.hs
+rm xmonad_config_README.md
+rm -r ~/.xmonad/deskspaces
 
 cp ~/.vimrc .
 cp ~/.vimrc.plug .
 cp ~/.bashrc .
 cp ~/.xmobarrc .
 cp ~/.xmonad/xmonad.hs .
+cp ~/.xmonad/xmonad_config_README.md .
+cp -r ~/.xmonad/deskspaces .
 
 git add .
 git commit -m "Updated dotfiles"
---
+++ xmonad_config_README.md
@@ -0,0 +1,4 @@
+### TODO
+-[ ] Spawn function for major project workspaces
+-[ ] dzen2 implementation of prior todo
+-[ ] keybindis to major workspaces(nodejs, rust, etc)
